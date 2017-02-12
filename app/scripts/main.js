566054b1152de882b3eba0526893a6b9e9c38a23
Rizchel Dayao <rdayao@us.ibm.com>
2017-02-12 12:23:57 -0600

    Updated files

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,9 @@
-window.addEventListener('keydown', (e) => {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if(!audio) return;
-  audio.currentTime = 0; //rewind sounds to the start
-  audio.play();
-  key.classList.add('playing');
-});
+const userInput = document.querySelectorAll('.controls input');
 
-function removeTransition(e) {
-  if(e.propertyName !== 'transform') return;
-  this.classList.remove('playing');
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
+userInput.forEach(input => input.addEventListener('change',handleUpdate));
+userInput.forEach(input => input.addEventListener('mousemove',handleUpdate));
